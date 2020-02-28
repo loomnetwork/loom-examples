9a7dabc98508a04c222fdb3367f36db6f3fc6617
Andrei Popescu <andrei.popescu@gmail.com>
2020-02-28 14:43:08 +0200

    Make EthSigning inherit from UniversalSigning
    Make EthSigning inherit from UniversalSigning

--- src/EthSigning/EthSigning.js
+++ src/EthSigning/EthSigning.js
@@ -1,140 +1,24 @@
-import {
-  NonceTxMiddleware,
-  SignedEthTxMiddleware,
-  CryptoUtils,
-  Client,
-  LoomProvider,
-  Address,
-  LocalAddress,
-  EthersSigner,
-  createDefaultTxMiddleware,
-  getMetamaskSigner
-} from 'loom-js'
-
-import { AddressMapper } from 'loom-js/dist/contracts'
 import { EventBus } from '../EventBus/EventBus'
 import networkConfigs from '../../network-configs.json'
+import { UniversalSigning } from '../UniversalSigning/UniversalSigning'
 import SimpleStoreJSON from '../../loom/build/contracts/SimpleStore.json'
 
-const Web3 = require('web3')
-
-export default class EthSigning {
-  async load (web3js) {
+export default class EthSigning extends UniversalSigning {
+  async load (web3Ethereum) {
     this.counter = 0
-    this.extdevConfig = networkConfigs.networks['extdev']
-    const client = this._createClient()
-    client.on('error', console.error)
-    const ethProvider = web3js.currentProvider
-    ethProvider.isMetaMask = true
-    const callerAddress = await this._setupSigner(client, ethProvider)
-    console.log('callerAddress: ' + callerAddress)
-    const loomProvider = await this._createLoomProvider(client, callerAddress)
-    let accountMapping = await this._loadMapping(callerAddress, client)
-    if (accountMapping === null) {
-      console.log('Create a new mapping')
-      const signer = getMetamaskSigner(ethProvider)
-      await this._createNewMapping(signer)
-      accountMapping = await this._loadMapping(callerAddress, client)
-      console.log(accountMapping)
-    } else {
-      console.log('mapping already exists')
-    }
-    console.log('mapping.ethereum: ' + accountMapping.ethereum.toString())
-    console.log('mapping.loom: ' + accountMapping.loom.toString())
+    this.extdevNetworkConfig = networkConfigs.networks['extdev']
+    const { web3Loom, accountMapping, client } = await super._load(web3Ethereum)
     this.accountMapping = accountMapping
-    this.web3js = web3js
-    this.web3loom = new Web3(loomProvider)
+    this.web3Ethereum = web3Ethereum
+    this.web3Loom = web3Loom
+    this.client = client
     await this._getContract()
     await this._filterEvents()
     await this._setValue()
   }
 
-  async _loadMapping (ethereumAccount, client) {
-    const mapper = await AddressMapper.createAsync(client, ethereumAccount)
-    let accountMapping = { ethereum: null, loom: null }
-    try {
-      const mapping = await mapper.getMappingAsync(ethereumAccount)
-      accountMapping = {
-        ethereum: mapping.from,
-        loom: mapping.to
-      }
-    } catch (error) {
-      console.error(error)
-      accountMapping = null
-    } finally {
-      mapper.removeAllListeners()
-    }
-    return accountMapping
-  }
-
-  async _createLoomProvider (client, callerAddress) {
-    const dummyKey = CryptoUtils.generatePrivateKey()
-    const publicKey = CryptoUtils.publicKeyFromPrivateKey(dummyKey)
-    const dummyAccount = LocalAddress.fromPublicKey(publicKey).toString()
-    const loomProvider = new LoomProvider(
-      client,
-      dummyKey,
-      () => client.txMiddleware
-    )
-    loomProvider.setMiddlewaresForAddress(callerAddress.local.toString(), client.txMiddleware)
-    loomProvider.callerChainId = callerAddress.chainId
-    // remove dummy account
-    loomProvider.accounts.delete(dummyAccount)
-    loomProvider._accountMiddlewares.delete(dummyAccount)
-    return loomProvider
-  }
-
-  async _setupSigner (loomClient, provider) {
-    const signer = getMetamaskSigner(provider)
-    const ethAddress = await signer.getAddress()
-    const callerAddress = new Address('eth', LocalAddress.fromHexString(ethAddress))
-
-    loomClient.txMiddleware = [
-      new NonceTxMiddleware(callerAddress, loomClient),
-      new SignedEthTxMiddleware(signer)
-    ]
-
-    return callerAddress
-  }
-
-  _createClient () {
-    const chainId = this.extdevConfig['chainId']
-    const writeUrl = this.extdevConfig['writeUrl']
-    const readUrl = this.extdevConfig['readUrl']
-    const client = new Client(chainId, writeUrl, readUrl)
-    return client
-  }
-
-  async _createNewMapping (signer) {
-    const ethereumAccount = await signer.getAddress()
-    const ethereumAddress = Address.fromString(`eth:${ethereumAccount}`)
-    const loomEthSigner = new EthersSigner(signer)
-    const privateKey = CryptoUtils.generatePrivateKey()
-    const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey)
-    const client = this._createClient()
-    client.txMiddleware = createDefaultTxMiddleware(client, privateKey)
-    const loomAddress = new Address(client.chainId, LocalAddress.fromPublicKey(publicKey))
-
-    const mapper = await AddressMapper.createAsync(client, loomAddress)
-    try {
-      await mapper.addIdentityMappingAsync(
-        ethereumAddress,
-        loomAddress,
-        loomEthSigner
-      )
-      client.disconnect()
-    } catch (e) {
-      if (e.message.includes('identity mapping already exists')) {
-      } else {
-        console.error(e)
-      }
-      client.disconnect()
-      return false
-    }
-  }
-
   async _getContract () {
-    this.contract = new this.web3loom.eth.Contract(SimpleStoreJSON.abi, SimpleStoreJSON.networks[this.extdevConfig['networkId']].address)
+    this.contract = new this.web3Loom.eth.Contract(SimpleStoreJSON.abi, SimpleStoreJSON.networks[this.extdevNetworkConfig['networkId']].address)
   }
 
   async _setValue () {
